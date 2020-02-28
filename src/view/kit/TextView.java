5a7dc1fe0cba84bd2ce7ecaf3e589b1ed83e532c
Bryan Lee <bryanleeminyuan@gmail.com>
2020-02-28 14:24:17 +0800

    Implement TextView
    A generic class that represents a body of text

---
+++ src/view/kit/TextView.java
@@ -0,0 +1,33 @@
+package view.kit;
+
+public class TextView extends View {
+    private String text;
+
+    boolean newLine = true;
+
+    public TextView(String text) {
+        this.text = text;
+    }
+
+    public TextView(String text, boolean newLine) {
+        this(text);
+        this.newLine = newLine;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public void render() {
+        if (newLine) {
+            System.out.println(text);
+        } else {
+            System.out.print(text);
+        }
+    }
+}
