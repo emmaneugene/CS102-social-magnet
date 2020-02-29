23fb656d2cda52ebde69442ad5c2ecaf75abdada
Bryan Lee <bryanleeminyuan@gmail.com>
2020-02-29 11:51:45 +0800

    Adapt PromptView to subclass TextView

--- src/view/kit/PromptView.java
+++ src/view/kit/PromptView.java
@@ -1,18 +1,20 @@
 package view.kit;
 
-public class PromptView extends View {
-    private String promptMessage;
+public class PromptView extends TextView {
+    /**
+     * @param text The message of the prompt
+     * @param promptIndicator The symbol between the prompt message and input
+     */
+    public PromptView(String text, String promptIndicator) {
+        super(String.format("%s %s ", text, promptIndicator));
+        this.newLine = false;
+    }
 
-    public PromptView(String promptMessage) {
-        this.promptMessage = promptMessage;
+    public PromptView(String text) {
+        this(text, ">");
     }
 
     public PromptView() {
-        this.promptMessage = "Enter your choice";
+        this("Enter your choice");
     }
-
-	@Override
-	public void renderMain() {
-		System.out.printf("%s > ", promptMessage);
-	}
 }
