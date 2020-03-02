71fd3c848489805cef066917173f0f298c75b63b
Bryan Lee <bryanleeminyuan@gmail.com>
2020-03-03 00:25:09 +0800

    Fix compile and run scripts
    `mvn package` will compile, test, then package the `.class` files.
    
    Therefore, if any test cases fail, we will not be able to run the `.jar`
    file on a clean directory.
    
    Fix by running the `.class` files directly.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # Create your own .env file
 # Look at .env-sample for an example of how to set it up
 source .env;
-export CLASSPATH="target/social-magnet-0.0.1.jar"
+export CLASSPATH="target/classes"
 export CLASSPATH="$CLASSPATH:$HOME/.m2/repository/mysql/mysql-connector-java/8.0.19/mysql-connector-java-8.0.19.jar"
 java -cp $CLASSPATH com.g1t11.socialmagnet.App;
