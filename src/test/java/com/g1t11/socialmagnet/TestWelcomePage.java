67fa025eae47b7d58a3f9e90a9f1d661737ff48a
charlieangriawan <charlieangriawan@hotmail.com>
2020-03-03 00:09:44 +0800

    Resolve conflict with master

--- src/test/java/com/g1t11/socialmagnet/TestWelcomePage.java
+++ src/test/java/com/g1t11/socialmagnet/TestWelcomePage.java
@@ -12,13 +12,13 @@ public class TestWelcomePage extends TestSocialMagnet {
     @Order(1)
     public void displayMenu() {
         App.shared().setAppName("Social Magnet");
-        App.shared().setFirstController(new WelcomePageController());
-        App.shared().getCurrentController().updateView();
+        App.shared().getNavigation().setFirstController(new WelcomePageController());
+        App.shared().getNavigation().getCurrentController().updateView();
 
         String expected = "\033[H\033[2J";
         expected += String.join("\n",
             "== Social Magnet :: Welcome ==",
-            "Good evening, anonymous!",
+            "Good morning, anonymous!",
             "1. Register",
             "2. Login",
             "3. Exit",
@@ -31,10 +31,10 @@ public class TestWelcomePage extends TestSocialMagnet {
     @Order(2)
     public void simulateInvalidInput() {
         App.shared().setAppName("Social Magnet");
-        App.shared().setFirstController(new WelcomePageController());
+        App.shared().getNavigation().setFirstController(new WelcomePageController());
         provideInput("5");
-        App.shared().getCurrentController().handleInput();
-        App.shared().getCurrentController().updateView();
+        App.shared().getNavigation().getCurrentController().handleInput();
+        App.shared().getNavigation().getCurrentController().updateView();
         String expectedError = "Please enter a choice between 1 & 3!";
         String errorMessage = getOutput().split("\\r?\\n")[1];
         Assertions.assertEquals(expectedError, errorMessage);
