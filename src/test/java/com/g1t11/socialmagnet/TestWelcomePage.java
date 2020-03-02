fa8489aee14d5dfbdad2f6dae14fa571f7b2d611
charlieangriawan <angriawan.charlie@gmail.com>
2020-03-02 23:57:36 +0800

    Implement testing utility

--- src/test/java/com/g1t11/socialmagnet/TestWelcomePage.java
+++ src/test/java/com/g1t11/socialmagnet/TestWelcomePage.java
@@ -1,50 +1,13 @@
 package com.g1t11.socialmagnet;
+import com.g1t11.socialmagnet.util.TestSocialMagnet;
+import com.g1t11.socialmagnet.controller.WelcomePageController;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.Assertions;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.PrintStream;
-
-import com.g1t11.socialmagnet.controller.WelcomePageController;
-
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class TestWelcomePage {
-    private final InputStream stdin = System.in;
-    private final PrintStream stdout = System.out;
-
-    private ByteArrayInputStream input;
-    private ByteArrayOutputStream output;
-
-    @BeforeEach
-    public void setOutputStreams() {
-        output = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(output));
-    }
-    
-    @AfterEach
-    public void restoreOutputStreams() {
-        System.setIn(stdin);
-        System.setOut(stdout);
-    }
-
-    private void provideInput(String data) {
-        input = new ByteArrayInputStream(data.getBytes());
-        System.setIn(input);
-    }
-
-    private String getOutput() {
-        return output.toString();
-    }
-
+// @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TestWelcomePage extends TestSocialMagnet {
     @Test
     @Order(1)
     public void displayMenu() {
@@ -61,7 +24,7 @@ public class TestWelcomePage {
             "3. Exit",
             "Enter your choice > "
         );
-        assertEquals(expected, getOutput());
+        Assertions.assertEquals(expected, getOutput());
     }
 
     @Test
@@ -73,7 +36,7 @@ public class TestWelcomePage {
         App.shared().getCurrentController().handleInput();
         App.shared().getCurrentController().updateView();
         String expectedError = "Please enter a choice between 1 & 3!";
-        String errorMessage = output.toString().split("\\r?\\n")[1];
-        assertEquals(expectedError, errorMessage);
+        String errorMessage = getOutput().split("\\r?\\n")[1];
+        Assertions.assertEquals(expectedError, errorMessage);
     }
 }
---
+++ src/test/java/com/g1t11/socialmagnet/util/TestSocialMagnet.java
@@ -0,0 +1,41 @@
+package com.g1t11.socialmagnet.util;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TestSocialMagnet {
+    private final InputStream stdin = System.in;
+    private final PrintStream stdout = System.out;
+
+    private static ByteArrayInputStream input;
+    private static ByteArrayOutputStream output;
+
+    @BeforeEach
+    public void setOutputStreams() {
+        output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+    }
+    
+    @AfterEach
+    public void restoreOutputStreams() {
+        System.setIn(stdin);
+        System.setOut(stdout);
+    }
+
+    public static void provideInput(String data) {
+        input = new ByteArrayInputStream(data.getBytes());
+        System.setIn(input);
+    }
+
+    public static String getOutput() {
+        return output.toString();
+    }
+}
