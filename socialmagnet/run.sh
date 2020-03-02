264f4e41e69c185306023253363fed28db9e8ff4
Bryan Lee <bryanleeminyuan@gmail.com>
2020-03-02 11:35:44 +0800

    Include Maven install of mysql JDBC

--- socialmagnet/run.sh
+++ socialmagnet/run.sh
@@ -1,4 +1,6 @@
 # Create your own .env file
 # Look at .env-sample for an example of how to set it up
 source .env;
-java -cp target/social-magnet-0.0.1.jar com.g1t11.socialmagnet.App;
+export CLASSPATH="target/social-magnet-0.0.1.jar"
+export CLASSPATH="$CLASSPATH:$HOME/.m2/repository/mysql/mysql-connector-java/8.0.19/mysql-connector-java-8.0.19.jar"
+java -cp $CLASSPATH com.g1t11.socialmagnet.App;
