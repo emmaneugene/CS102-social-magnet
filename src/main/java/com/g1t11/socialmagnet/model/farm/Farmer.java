15c88fc3988b5f883712872372fd434e7beb8d55
Emmanuel Oh <emmaneugene@outlook.com>
2020-03-08 20:32:32 +0800

    Initial commit

--- src/main/java/com/g1t11/socialmagnet/model/farm/Farmer.java
+++ src/main/java/com/g1t11/socialmagnet/model/farm/Farmer.java
@@ -1,5 +1,6 @@
 package com.g1t11.socialmagnet.model.farm;
 
+import com.g1t11.socialmagnet.model.social.User;
 import java.util.ArrayList;
 
 /**
@@ -11,11 +12,88 @@ import java.util.ArrayList;
  * - inventory : ArrayList<InventoryItem>
  * - stolenCrops : ArrayList<Stealing>
  */
-public class Farmer {
-    private String rank;
-    private int exp;
-    private int wealth;
-    private ArrayList<Plot> farmland;
-    private ArrayList<InventoryItem> inventory;
-    private ArrayList<Stealing> stolenCrops;
+public class Farmer extends User {
+    private String rank = null;
+
+    private int exp = 0;
+
+    private int wealth = 50;
+
+    private ArrayList<Plot> farmland = null;
+
+    private ArrayList<InventoryItem> inventory = null;
+
+    private ArrayList<Stealing> stolenCrops = null;
+
+    public Farmer() {}
+
+    public Farmer(String username, String fullName) {
+        super(username, fullName);
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public String setRank() {
+        //TODO: set farmer's rank based on exp, whenever the exp changes
+        return null;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+
+    // convenience method to add exp
+    public void addExp(int exp_earned) {
+        exp += exp_earned;
+    }
+
+    public int getWealth() {
+        return wealth;
+    }
+
+    public void setWealth(int wealth) {
+        this.wealth = wealth;
+    }
+
+    public ArrayList<Plot> getFarmland() {
+        return farmland;
+    }
+
+    public void setFarmland(ArrayList<Plot> farmland) {
+        this.farmland = farmland;
+    }
+
+    public void addPlot(Plot plot) {
+        farmland.add(plot);
+    }
+
+    public ArrayList<InventoryItem> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(ArrayList<InventoryItem> inventory) {
+        this.inventory = inventory;
+    }
+
+    public void addInventoryItem(InventoryItem item) {
+        inventory.add(item);
+    }
+
+    public ArrayList<Stealing> getStolenCrops() {
+        return stolenCrops;
+    }
+
+    public void setStolenCrops(ArrayList<Stealing> stolenCrops) {
+        this.stolenCrops = stolenCrops;
+    }
+
+    public void addStolenCrop(Stealing stolenCrop) {
+        stolenCrops.add(stolenCrop);
+    }
 }
--- src/main/java/com/g1t11/socialmagnet/model/farm/Gift.java
+++ src/main/java/com/g1t11/socialmagnet/model/farm/Gift.java
@@ -11,7 +11,33 @@ import java.util.Date;
  */
 public class Gift {
     private Farmer sender;
+
     private Farmer recipient;
+    
     private Crop crop;
+    
     private Date timestamp;
+
+    public Gift(Farmer sender, Farmer recipient, Crop crop) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.crop = crop;
+        timestamp = new Date();
+    }
+
+    public Farmer getSender() {
+        return sender;
+    }
+
+    public Farmer getRecipient() {
+        return recipient;
+    }
+
+    public Crop getCrop() {
+        return crop;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
 }
