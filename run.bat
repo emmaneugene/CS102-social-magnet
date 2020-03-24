85c3d5747d91c7481c6792dc6224761d824684f0
Emmanuel Oh <emmaneugene@outlook.com>
2020-03-24 10:47:28 +0800

    Write batch scripts for windows

--- run.bat
+++ run.bat
@@ -1,4 +1,3 @@
-source .env;
-export CLASSPATH="target/classes"
-export CLASSPATH="$CLASSPATH:$HOME/.m2/repository/mysql/mysql-connector-java/8.0.19/mysql-connector-java-8.0.19.jar"
-java -cp target/classes; com.g1t11.socialmagnet.App;
+call load-env.bat
+
+java -cp target/classes;/Users/User/.m2/repository/mysql/mysql-connector-java/8.0.19/mysql-connector-java-8.0.19.jar com.g1t11.socialmagnet.App
