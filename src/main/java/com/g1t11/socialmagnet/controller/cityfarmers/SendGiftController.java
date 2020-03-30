d7c16bbc10f28bec7ce69cc27bc1fd5eb6ff6c98
Bryan Lee <bryanleeminyuan@gmail.com>
2020-03-31 05:05:15 +0800

    Use arrays instead of collections

--- src/main/java/com/g1t11/socialmagnet/controller/cityfarmers/SendGiftController.java
+++ src/main/java/com/g1t11/socialmagnet/controller/cityfarmers/SendGiftController.java
@@ -1,5 +1,6 @@
 package com.g1t11.socialmagnet.controller.cityfarmers;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -77,34 +78,34 @@ public class SendGiftController extends CityFarmersController {
 
         Map<String, Boolean> sentGifts = farmerLoadDAO.sentGiftToUsersToday(me.getUsername(), recipients);
         // Reduce the map into a list containing all invalid recipients.
-        List<String> usersSentTo = sentGifts.entrySet().stream()
+        String[] usersSentTo = sentGifts.entrySet().stream()
             .filter((Entry<String, Boolean> entry) -> entry.getValue())
             .map((Entry<String, Boolean> entry) -> entry.getKey())
-            .collect(Collectors.toList());
+            .toArray(size -> new String[size]);
 
-        if (usersSentTo.size() == 0) return true;
+        if (usersSentTo.length == 0) return true;
 
         displayAlreadySentTodayError(usersSentTo);
         return false;
     }
 
-    private void displayAlreadySentTodayError(List<String> usersSentTo) {
-        int sentCount = usersSentTo.size();
+    private void displayAlreadySentTodayError(String[] usersSentTo) {
+        int sentCount = usersSentTo.length;
         // Prepare the names for Painter::paintf
-        List<String> prepName = usersSentTo.stream()
+        String[] prepName = Arrays.stream(usersSentTo)
             .map((String name) -> String.format("[{%s}]", name))
-            .collect(Collectors.toList());
+            .toArray(size -> new String[size]);
 
         StringBuilder sb = new StringBuilder("[{Already sent a gift to");
         if (sentCount == 1) {
             sb.append(" ")
-            .append(prepName.get(0))
+            .append(prepName[0])
             .append(" today.}]");
         } else {
             for (int i = 0; i < sentCount - 1; i++) {
-                sb.append(" ").append(prepName.get(i)).append(",");
+                sb.append(" ").append(prepName[i]).append(",");
             }
-            sb.append(" and ").append(prepName.get(sentCount - 1)).append(" today.}]");
+            sb.append(" and ").append(prepName[sentCount - 1]).append(" today.}]");
         }
         view.setStatus(Painter.paintf(sb.toString(), Painter.Color.RED, Painter.Color.BLUE));
     }
