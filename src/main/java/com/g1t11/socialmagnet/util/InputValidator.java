e596dfc699da7157d32280e203184cfc4f72d3bd
Zhi An <yuenzhian@gmail.com>
2020-04-02 18:32:40 +0800

    Add javadocs to util

--- src/main/java/com/g1t11/socialmagnet/util/InputValidator.java
+++ src/main/java/com/g1t11/socialmagnet/util/InputValidator.java
@@ -1,6 +1,14 @@
 package com.g1t11.socialmagnet.util;
 
+/**
+ * This class consists exclusively of static methods that validate inputs.
+ */
 public class InputValidator {
+    /**
+     * 
+     * @param input The input string used for validation.
+     * @return If input is alphanumeric, it will return true, else return false.
+     */
     public static boolean isAlphanumeric(String input) {
         return input != null && input.matches("^[a-zA-Z0-9]+$");
     }
--- src/main/java/com/g1t11/socialmagnet/util/TextUtils.java
+++ src/main/java/com/g1t11/socialmagnet/util/TextUtils.java
@@ -2,7 +2,19 @@ package com.g1t11.socialmagnet.util;
 
 import java.util.List;
 
+/**
+ * This class consists exclusively of static methods that generate the
+ * appropriate string accordingly to the counts of words/numbers/lists.
+ */
 public class TextUtils {
+
+    /**
+     * Build appropriate string whether if it is singular or plural.
+     * @param count The number of words.
+     * @param singular The string to use if it is singular.
+     * @param plural The string to use if it is plural.
+     * @return Generated appropriate string.
+     */
     public static String countedWord(int count,
             String singular, String plural) {
         if (count == 1) {
@@ -11,6 +23,11 @@ public class TextUtils {
         return String.format("%d %s", count, plural);
     }
 
+    /**
+     * Build appropriate string with ',' according to the input number.
+     * @param number The input number
+     * @return Generated appropriate string wuth ','.
+     */
     public static String prettyNumber(int number) {
         if (number < 0) return "-" + prettyNumber(-number);
         if (number < 1000) return Integer.toString(number);
@@ -24,6 +41,12 @@ public class TextUtils {
         return sb.toString();
     }
 
+    /**
+     * Build approriate string according to array of items.
+     * @param <T> Input of generatic type.
+     * @param items The input array of items.
+     * @return Generated appropriate string.
+     */
     public static <T> String prettyList(T[] items) {
         if (items == null) return null;
         int length = items.length;
@@ -44,6 +67,12 @@ public class TextUtils {
         return sb.toString();
     }
 
+    /**
+     * Build approriate string according to list of items.
+     * @param <T> Input of List.
+     * @param items The input list of items.
+     * @return Generated appropriate string.
+     */
     public static <T> String prettyList(List<? extends T> items) {
         return prettyList(items.toArray());
     }
