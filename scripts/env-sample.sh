34feadf527f92d33fb8bb5e1e00ea6d5f2f1736a
Bryan Lee <42545742+bryanmylee@users.noreply.github.com>
2020-04-07 11:06:18 +0800

    Update env-sample.sh

--- scripts/env-sample.sh
+++ scripts/env-sample.sh
@@ -1,5 +1,5 @@
 # Use these environment variables to define the database credentials
-# and save the file as .env
+# and save the file as env.sh
 export DB_NAME="magnet"
 export DB_USER="admin"
 export DB_PASS="mysupersecretkey"
