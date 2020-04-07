24380a5f6886216088d37f25910450c128006c2d
Bryan Lee <42545742+bryanmylee@users.noreply.github.com>
2020-04-07 11:05:57 +0800

    Update env-sample.bat

--- scripts/env-sample.bat
+++ scripts/env-sample.bat
@@ -1,5 +1,6 @@
-:: Run this batch script to set databse credentials as environment variables
-:: Replace DB_NAME, DB_USER and DB_PASS according to your own database settings
+:: Run this batch script to set database credentials as environment variables
+:: Replace DB_NAME, DB_USER and DB_PASS according to your own database settings,
+:: and save as env.bat
 :: The string values should NOT be encased in any inverted commas
 set DB_NAME=magnet
 set DB_USER=admin
